50264f15c8dccbb855843feec007ff5ed68f72d2
Jake <jacobj@spacelift.io>
2023-11-08 20:31:57 -0800

    actually fixed the spaces

--- spacelift-resources/main.tf
+++ spacelift-resources/main.tf
@@ -1,6 +1,6 @@
 resource "spacelift_stack" "vpc-stack" {
   administrative               = false
-  space_id                     = "stack-dependencies-demo"
+  space_id                     = "stack-dependencies-demo-01HES50MW0R4XW1AME0BPP8YVY"
   branch                       = "main"
   description                  = "This stack creates a VPC"
   name                         = "vpc-stack"
@@ -12,7 +12,7 @@ resource "spacelift_stack" "vpc-stack" {
 
 resource "spacelift_stack" "ec2-stack" {
   administrative               = false
-  space_id                     = "stack-dependencies-demo"
+  space_id                     = "stack-dependencies-demo-01HES50MW0R4XW1AME0BPP8YVY"
   branch                       = "main"
   description                  = "This stack creates an ec2 instanc"
   name                         = "ec2-stack"
@@ -24,7 +24,7 @@ resource "spacelift_stack" "ec2-stack" {
 
 resource "spacelift_stack" "ansible-stack" {
   administrative               = false
-  space_id                     = "stack-dependencies-demo"
+  space_id                     = "stack-dependencies-demo-01HES50MW0R4XW1AME0BPP8YVY"
   branch                       = "main"
   description                  = "This stack configures the deployed ec2 using ansible"
   name                         = "ansible-stack"
