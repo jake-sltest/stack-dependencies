96561c177cd4f299f1ce6eedcbc12df0846ac856
Jake <jacobj@spacelift.io>
2023-11-08 20:25:19 -0800

    first commit for scaffolding

---
+++ spacelift-resources/main.tf
@@ -0,0 +1,35 @@
+resource "spacelift_stack" "vpc-stack" {
+  administrative               = false
+  space_id                     = data.spacelift_current_space.this.id
+  branch                       = "main"
+  description                  = "This stack creates a VPC"
+  name                         = "vpc-stack"
+  project_root                 = "/infrastructure/terraform/vpc"
+  repository                   = "stack-dependencies"
+  terraform_smart_sanitization = true
+  autodeploy        = true
+}
+
+resource "spacelift_stack" "ec2-stack" {
+  administrative               = false
+  space_id                     = data.spacelift_current_space.this.id
+  branch                       = "main"
+  description                  = "This stack creates an ec2 instanc"
+  name                         = "ec2-stack"
+  project_root                 = "/infrastructure/terraform/instances"
+  repository                   = "stack-dependencies"
+  terraform_smart_sanitization = true
+  autodeploy        = true
+}
+
+resource "spacelift_stack" "ansible-stack" {
+  administrative               = false
+  space_id                     = data.spacelift_current_space.this.id
+  branch                       = "main"
+  description                  = "This stack configures the deployed ec2 using ansible"
+  name                         = "ansible-stack"
+  project_root                 = "/ansible"
+  repository                   = "stack-dependencies"
+  terraform_smart_sanitization = true
+  autodeploy        = true
+}
