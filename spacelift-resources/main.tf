3468f6694c85d3de30c6ff87158ab956125329dc
Jake <jacobj@spacelift.io>
2023-11-13 19:36:25 -0800

    fixed output name

--- spacelift-resources/main.tf
+++ spacelift-resources/main.tf
@@ -60,8 +60,8 @@ resource "spacelift_stack_dependency" "ec2-depends-on-vpc" {
 
 resource "spacelift_stack_dependency_reference" "ec2-vpc-output" {
   stack_dependency_id = spacelift_stack_dependency.ec2-depends-on-vpc.id
-  output_name         = "sd_subnet"
-  input_name          = "sd_subnet"
+  output_name         = "subnetId"
+  input_name          = "subnetId"
 }
 
 resource "spacelift_stack_dependency" "ansible-depends-on-ec2" {
