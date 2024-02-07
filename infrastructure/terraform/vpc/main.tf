2cda2f064db759b41645f9004278ffac16e8a587
Jake <jacobj@spacelift.io>
2024-02-07 15:43:15 -0600

    new subnet

--- infrastructure/terraform/vpc/main.tf
+++ infrastructure/terraform/vpc/main.tf
@@ -20,7 +20,7 @@ resource "aws_vpc" "sd_vpc" {
   }
 }
 
-resource "aws_subnet" "sd_subnet" {
+resource "aws_subnet" "sd_subnet_new" {
     vpc_id = aws_vpc.sd_vpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
