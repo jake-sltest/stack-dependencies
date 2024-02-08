e178637abb2e43e88e5b4209a066abf5680a5eec
Jake <jacobj@spacelift.io>
2024-02-08 11:16:13 -0600

    subnet name change

--- infrastructure/terraform/vpc/main.tf
+++ infrastructure/terraform/vpc/main.tf
@@ -20,7 +20,7 @@ resource "aws_vpc" "sd_vpc" {
   }
 }
 
-resource "aws_subnet" "sd_subnet_new" {
+resource "aws_subnet" "sd_subnet_new_change" {
     vpc_id = aws_vpc.sd_vpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
@@ -32,5 +32,5 @@ resource "aws_subnet" "sd_subnet_new" {
 
 output "subnetId" {
   description = "ID of the Subnet"
-  value       = aws_subnet.sd_subnet_new.id
+  value       = aws_subnet.sd_subnet_new_change.id
 }
