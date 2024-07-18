e92f7f2bac82918e94bb3488e49a474ff3ff05b3
Jake <jacobj@spacelift.io>
2024-07-18 11:35:09 -0700

    fixed name

--- infrastructure/terraform/vpc/main.tf
+++ infrastructure/terraform/vpc/main.tf
@@ -20,7 +20,7 @@ resource "aws_default_vpc" "sd_vpc" {
 }
 
 resource "aws_subnet" "sd_subnet" {
-    vpc_id = aws_vpc.sd_vpc.id
+    vpc_id = aws_default_vpc.sd_vpc.id
     cidr_block = "10.0.0.0/16"
     map_public_ip_on_launch = "true"
 
