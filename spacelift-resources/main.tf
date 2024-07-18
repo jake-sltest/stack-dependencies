f5960d3c1dc2d6036709afc76009e562bd91c2fa
Jake <jacobj@spacelift.io>
2024-07-18 10:07:12 -0700

    comment uneeded ansible context

--- spacelift-resources/main.tf
+++ spacelift-resources/main.tf
@@ -70,36 +70,36 @@ resource "spacelift_stack_dependency_reference" "ansible-ec2-output" {
   input_name          = "ec2Id"
 }
 
-#Create ansible context
+# #Create ansible context
 
-resource "spacelift_context" "ansible-context" {
-  description = "Context for Terraform-Ansible workflow demo"
-  name        = "Ansible context test "
-  space_id    = "stack-dependencies-demo-01HES50MW0R4XW1AME0BPP8YVY"
-  labels      = ["autoattach:sd-demo"]
-}
+# resource "spacelift_context" "ansible-context" {
+#   description = "Context for Terraform-Ansible workflow demo"
+#   name        = "Ansible context test "
+#   space_id    = "stack-dependencies-demo-01HES50MW0R4XW1AME0BPP8YVY"
+#   labels      = ["autoattach:sd-demo"]
+# }
 
-# RSA key of size 4096 bits
-resource "tls_private_key" "rsa-ansible" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# # RSA key of size 4096 bits
+# resource "tls_private_key" "rsa-ansible" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-resource "aws_key_pair" "ansible-key" {
-  key_name   = "tf-ansible-workflow-key"
-  public_key = tls_private_key.rsa-ansible.public_key_openssh
-}
+# resource "aws_key_pair" "ansible-key" {
+#   key_name   = "tf-ansible-workflow-key"
+#   public_key = tls_private_key.rsa-ansible.public_key_openssh
+# }
 
-resource "spacelift_mounted_file" "ansible-key" {
-  context_id    = spacelift_context.ansible-context.id
-  relative_path = "tf-ansible-key.pem"
-  content       = base64encode(nonsensitive(tls_private_key.rsa-ansible.private_key_pem))
-  write_only    = true
-}
+# resource "spacelift_mounted_file" "ansible-key" {
+#   context_id    = spacelift_context.ansible-context.id
+#   relative_path = "tf-ansible-key.pem"
+#   content       = base64encode(nonsensitive(tls_private_key.rsa-ansible.private_key_pem))
+#   write_only    = true
+# }
 
-resource "spacelift_environment_variable" "ansible_private_key_file" {
-  context_id = spacelift_context.ansible-context.id
-  name       = "ANSIBLE_PRIVATE_KEY_FILE"
-  value      = "/mnt/workspace/tf-ansible-key.pem"
-  write_only = false
-}
+# resource "spacelift_environment_variable" "ansible_private_key_file" {
+#   context_id = spacelift_context.ansible-context.id
+#   name       = "ANSIBLE_PRIVATE_KEY_FILE"
+#   value      = "/mnt/workspace/tf-ansible-key.pem"
+#   write_only = false
+# }
