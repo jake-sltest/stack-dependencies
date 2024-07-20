c36828bf95a0fe2e027a3859b89777618b98ff28
Jake <jacobj@spacelift.io>
2024-07-20 14:09:57 -0700

    updated provider version

--- spacelift-resources/providers.tf
+++ spacelift-resources/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     spacelift = {
       source  = "spacelift-io/spacelift"
-      version = "1.6.0"
+      version = "1.14.0"
     }
     aws = {
       source  = "hashicorp/aws"
