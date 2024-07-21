50976c4ce6690afbf1d92d9335dba0e77a2f84cd
Jake <jacobj@spacelift.io>
2024-07-21 15:47:34 -0700

    added security group for ssh

--- infrastructure/instances/main.tf
+++ infrastructure/instances/main.tf
@@ -12,6 +12,51 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_security_group" "allow_access" {
+  name        = "stack-dependency-demo"
+  description = "Allow SSH, HTTP and HTTPS traffic"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "HTTP"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Stack Dependency EC2"
+  }
+}
+
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ec2-key-pair"
   public_key = file(var.public_key)
@@ -25,6 +70,6 @@ resource "aws_instance" "sd_instance" {
   associate_public_ip_address = true
   
   tags = {
-    Name = "Stack Dependency EC2 change"
+    Name = "Stack Dependency EC2"
   }
 }
