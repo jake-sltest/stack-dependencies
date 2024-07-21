2d2d1bc1ff3fb64d3bf0b625d411d60e4bc6d12b
Jake <jacobj@spacelift.io>
2024-07-21 15:55:42 -0700

    added ec2 to security group

--- infrastructure/instances/main.tf
+++ infrastructure/instances/main.tf
@@ -68,7 +68,8 @@ resource "aws_instance" "sd_instance" {
   subnet_id = var.subnetId
   key_name                    = aws_key_pair.ssh_key.key_name
   associate_public_ip_address = true
-  
+  vpc_security_group_ids = [aws_security_group.allow_access.id]
+
   tags = {
     Name = "Stack Dependency EC2"
   }
