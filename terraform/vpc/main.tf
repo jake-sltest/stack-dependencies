5ba403e21545ed802c66c68b756d3831906d0281
Jake <jacobj@spacelift.io>
2025-08-01 12:08:14 -0700

    fixed reference

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,13 +1,11 @@
 resource "aws_default_vpc" "sd_vpc" {
-  cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "Stack Dependency VPC"
   }
 }
 
 resource "aws_subnet" "sd_subnet" {
-  vpc_id                  = aws_vpc.sd_vpc.id
+  vpc_id                  = aws_vpc.aws_default_vpc.id
   cidr_block              = "10.0.0.0/16"
   map_public_ip_on_launch = "true"
 
