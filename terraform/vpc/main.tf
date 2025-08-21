15f7668805c7223657a1214887a13ee208e98e77
Jake <jacobj@spacelift.io>
2025-08-21 10:03:22 -0700

    added CIDR

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -7,6 +7,8 @@ resource "aws_default_vpc" "sd_vpc" {
 resource "aws_subnet" "sd_subnet" {
   vpc_id                  = aws_default_vpc.sd_vpc.id
   map_public_ip_on_launch = "true"
+  cidr_block              = "0.0.0.0/0"
+
 
   tags = {
     demo = "Stack Dependency Subnet"
