0555f98125923be54b76c541202282409d32313a
jacobjSL <118396550+jacobjSL@users.noreply.github.com>
2025-08-29 15:06:54 -0700

    Update main.tf

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "sd_subnet" {
 resource "aws_subnet" "sd_subnet_second" {
   vpc_id                  = aws_vpc.sd_vpc.id
   map_public_ip_on_launch = "true"
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.0.0/24"
 
 
   tags = {
