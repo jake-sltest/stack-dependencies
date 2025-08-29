25a705a980f3e2b26c462599ed03068ab5c03ae3
jacobjSL <118396550+jacobjSL@users.noreply.github.com>
2025-08-29 15:04:01 -0700

    Update main.tf

--- terraform/instances/main.tf
+++ terraform/instances/main.tf
@@ -45,6 +45,54 @@ resource "aws_security_group" "allow_access" {
   }
 }
 
+resource "aws_security_group" "allow_access_second" {
+  name        = "stack-dependency-demo"
+  description = "Allow SSH, HTTP and HTTPS traffic"
+  vpc_id      = var.vpcId
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Stack Dependency EC2"
+  }
+}
+
+
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ec2-key-pair"
   public_key = file(var.public_key)
