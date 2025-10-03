95506f1be81fda1461d2ea2c074eb4e317dd2b15
Jake <jacobj@spacelift.io>
2025-10-03 10:20:56 -0700

    allow all traffic

--- main.tf
+++ main.tf
@@ -12,68 +12,44 @@ provider "aws" {
 }
 
 
-resource "aws_vpc" "sd_vpc" {
+resource "aws_vpc" "worker_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "Stack Dependency VPC"
+    Name = "worker VPC"
   }
 }
 
 
-resource "aws_subnet" "sd_subnet" {
-  vpc_id                  = aws_vpc.sd_vpc.id
+resource "aws_subnet" "worker_subnet" {
+  vpc_id                  = aws_vpc.worker_vpc.id
   map_public_ip_on_launch = "true"
   cidr_block              = "10.0.1.0/24"
 
 
 
   tags = {
-    demo = "Stack Dependency Subnet"
+    demo = "worker Subnet"
   }
 }
 
-resource "aws_security_group" "allow_access" {
+resource "aws_security_group" "worker_sg" {
   name        = "stack-dependency-demo"
-  description = "Allow SSH, HTTP and HTTPS traffic"
-  vpc_id      = aws_vpc.sd_vpc.id
+  description = "Allow all traffic"
+  vpc_id      = aws_vpc.worker_vpc.id
 
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "Stack Dependency EC2"
   }
